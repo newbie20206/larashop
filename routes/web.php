582078abe47af099e0921ba8873808dfd8faf5bc
newbie91521 <newbie91521@163.com>
2019-12-02 19:36:25 +0800

    3.3. 收货地址列表(在Tinker中使用数据工厂生成假数据)

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('/', 'PagesController@root')->name('root');
 // 在之前的路由里加上一个 verify 参数
 Auth::routes(['verify' => true]);
 
+// auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
