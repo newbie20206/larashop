ae38075aec16b8479615476e3fb51729926de603
newbie91521 <newbie91521@163.com>
2019-12-05 18:21:07 +0800

    3.4. 新建收货地址(Vue实现省市区联动组件、设置中文语言包将表单错误提示改为中文)

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -6,16 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserAddress extends Model
 {
-//    protected $fillable = [
-//        'province',
-//        'city',
-//        'district',
-//        'address',
-//        'zip',
-//        'contact_name',
-//        'contact_phone',
-//        'last_used_at',
-//    ];
+    protected $fillable = [
+        'province',
+        'city',
+        'district',
+        'address',
+        'zip',
+        'contact_name',
+        'contact_phone',
+        'last_used_at',
+    ];
+
     protected $dates = ['last_used_at'];
 
     public function user()
