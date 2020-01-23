341d85582be1204721153cee3b9f6814155ce2a2
newbie91521 <newbie91521@163.com>
2020-01-23 14:39:33 +0800

    9.5. 优化优惠券模块(一张优惠券一个用户只能使用一次、订单金额不满足优惠券最低金额、关闭订单减少优惠券用量)

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -11,18 +11,18 @@ class CouponCodesController extends Controller
 {
     /**判断优惠券是否有效
      * @param $code
+     * @param Request $request
      * @return mixed
-     * @throws CouponCodeUnavailableException
-     * Author: sai
+     * @throws CouponCodeUnavailableException Author: sai
      * DateTime: 2020/1/23 11:02 上午
      */
-    public function show($code) {
+    public function show($code, Request $request) {
         // 判断优惠券是否存在
         if (!$record = CouponCode::where('code', $code)->first()) {
             throw new CouponCodeUnavailableException('优惠券不存在');
         }
 
-        $record->checkAvailable();
+        $record->checkAvailable($request->user());
 
         return $record;
     }
