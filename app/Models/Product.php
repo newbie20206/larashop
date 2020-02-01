78aad73346924b41230e668de310f0dcdc3cbf29
newbie91521 <newbie91521@163.com>
2020-02-01 20:37:05 +0800

    6.1. 商品属性模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -47,4 +47,22 @@ class Product extends Model
     {
         return $this->hasOne(CrowdfundingProduct::class);
     }
+
+    //关联属性
+    public function properties()
+    {
+        return $this->hasMany(ProductProperty::class);
+    }
+
+    //合并相同的商品属性名称
+    public function getGroupedPropertiesAttribute()
+    {
+        return $this->properties
+            // 按照属性名聚合，返回的集合的 key 是属性名，value 是包含该属性名的所有属性集合
+            ->groupBy('name')
+            ->map(function ($properties) {
+                // 使用 map 方法将属性集合变为属性值集合
+                return $properties->pluck('value')->all();
+            });
+    }
 }
