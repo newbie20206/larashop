d259909c53eb7d2a672e393d832e638a506a1352
newbie91521 <newbie91521@163.com>
2020-02-05 23:40:11 +0800

    初始化秒杀模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,9 +10,12 @@ class Product extends Model
 {
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
+    const TYPE_SECKILL = 'seckill';
+
     public static $typeMap = [
         self::TYPE_NORMAL  => '普通商品',
         self::TYPE_CROWDFUNDING => '众筹商品',
+        self::TYPE_SECKILL => '秒杀商品',
     ];
 
     protected $fillable = [
@@ -111,4 +114,11 @@ class Product extends Model
     {
         return $query->whereIn('id', $ids)->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $ids)));
     }
+
+    //关联秒杀商品
+    public function seckill()
+    {
+        return $this->hasOne(SeckillProduct::class);
+    }
+
 }
